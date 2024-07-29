8e7d0d872735dc249250993c194c2d79c03ab67a
keithdperez <xkeithperezx@gmail.com>
2024-07-29 16:32:04 -0500

    Quartz sync: Jul 29, 2024, 4:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,18 +22,18 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Sansita Bold",
+        body: "Nunito Sans",
         code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#e7d9cb",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#c2c8cc",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
